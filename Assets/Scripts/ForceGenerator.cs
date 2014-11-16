5e4260c4bce7c0484b1524dc75347d7c35348de6
Matt Allen <matt_allen@utexas.edu>
2014-11-15 18:38:16 -0600

    Added glow to force generator.

--- Assets/Scripts/ForceGenerator.cs
+++ Assets/Scripts/ForceGenerator.cs
@@ -7,11 +7,27 @@ public class ForceGenerator : MonoBehaviour
 	public float ForceStrength = 1f;
 	public bool Active = false;
 
+	public SpriteRenderer Sprite;
+	public Color DefaultColor = Color.white;
+	public Color PositiveColor = Color.blue;
+	public Color NegativeColor = Color.red;
+
 	private List<GameObject> intersecting = new List<GameObject> ();
+	private Color currentColor;
+
+	void Start ()
+	{
+		if (!this.Sprite) 
+			this.Sprite = GetComponent<SpriteRenderer> ();
+	}
 
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
+		currentColor = Active ? (ForceStrength > 0 ? PositiveColor : NegativeColor) : DefaultColor;
+		if (this.Sprite) 
+			this.Sprite.color = currentColor;
+
 		if (Active) {
 			var objects = FindObjectsOfType<MagneticObject> ();
 			foreach (var o in objects) {
